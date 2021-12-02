e8c721bcf6337b88dcd3b15d3565efb65ca521eb
CJ Karkut <cjkarkut@gmail.com>
2021-12-02 10:21:00 -0600

    new variable

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-let bobby = 'Like this, Bobby?';
 function greet() {
+let bobby = 'Like this, Bobby? Lemmy know man. I need to know. I can handle the truth! ';
     alert(" ummmmm " + bobby)
 }
 greet();
